9dea6c61417b67130c16e764487c7d51bcd78b1a
Animarroquin <alucard224@hotmail.com>
2024-05-24 00:54:45 -0600

    Update main.py
    Improved print message readability using f-strings

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
     else:
-        print("Se debe indicar el fichero como primer argumento")
-        print("El segundo argumento indica si se quieren eliminar duplicados")
+        print(f"Se debe indicar el fichero como primer argumento\nEl segundo argumento indica si se quieren eliminar duplicados")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
